3f6ea6f6825cab95abcc16ee6ce807a8937416fb
Wang Xiangyu <u0909122@nus.edu.sg>
2010-09-20 10:03:17 +0000

    MainCommandExecutor.h

--- commandExecutors/MainCommandExecutor.h
+++ commandExecutors/MainCommandExecutor.h
@@ -5,6 +5,14 @@ public:
 	MainCommandExecutor(){
 		executors[ADD] = new AddCommandExecutor();
 		executors[FINISH] = new FinishCommandExecutor();
+		executors[EDIT] = new EditCommandExecutor();
+		executors[EXPORT] = new ExportCommandExecutor();
+		executors[IMPORT] = new ImportCommandExecutor();
+		executors[PRI] = new PriCommandExecutor();
+		executors[LS] = new LsCommandExecutor();
+		executors[TASK] = new TaskCommandExecutor();
+		executors[RM] = new RmCommandExecutor();
+		//executors[CRON] = new CronCommandExecutor();
 	}
 	Result *executeCommand(Command *command){
 		return executors[command->method]->executeCommand(command);
