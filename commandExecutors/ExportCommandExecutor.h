02687b3149c6bac7144335b6d467c1d806e65287
Wang Xiangyu <u0909122@nus.edu.sg>
2010-09-20 09:38:52 +0000

    ExportCommandExecutor.h

---
+++ commandExecutors/ExportCommandExecutor.h
@@ -0,0 +1,46 @@
+#include <fstream>
+
+class ExportCommandExecutor: public CommandExecutor
+{
+public:
+	Result *executeCommand (Command* command)
+	{
+		//export to file record.xml
+		if (command->method == EXPORT)
+		{
+			//int numberOfTasks = mainTaskList->getSerial();
+
+			map<int, Task*> tasks = mainTaskList->getTaskMap();
+
+			ofstream writeFile("record.xml");
+
+			//file head
+			writeFile<<"<taskList>"<<endl;
+
+			//file body
+			for (map<int, Task*>::iterator iter = tasks.begin(); iter!=tasks.end(); iter++)
+			{
+				//task head
+				writeFile<<"	<task>"<<endl;
+
+				//task body
+				writeFile<<"		<deadline>"<<iter->second->getDeadline()<<"</deadline>"<<endl;
+				writeFile<<"		<priority>"<<iter->second->getPriority()<<"</priority>"<<endl;
+				writeFile<<"		<description>"<<iter->second->getDescription()<<"</description>"<<endl;
+				writeFile<<"		<cronFreq>"<<iter->second->getCronFreq()<<"</cronFreq>"<<endl;
+				writeFile<<"		<isFinished>"<<iter->second->getIsFinished()<<"</isFinished>"<<endl;
+				writeFile<<"		<serialNumber>"<<iter->second->getSerialNumber()<<"</serialNumber>"<<endl;
+				writeFile<<"		<group>"<<iter->second->getGroup()<<"</group>"<<endl;
+
+				//task end
+				writeFile<<"	</task>"<<endl;
+			}
+			
+			//file end
+			writeFile<<"</taskList>"<<endl;
+
+			writeFile.close();
+		}
+		return new Result();
+	}
+};
